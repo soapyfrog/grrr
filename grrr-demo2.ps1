ac6fed544d14bb26c6853ca765f40d8c0b3769e9
adrian <adrian@91d854f5-e528-0410-816d-da5f51aa4fc4>
2006-12-10 10:48:58 +0000

    Improved demo a bit. Now wraps, has some fuel dumps and missile stations, and have a put in a rocket for effect. Still *painfully* slow though.

--- grrr-demo2.ps1
+++ grrr-demo2.ps1
@@ -15,23 +15,30 @@ cls
 # Parallelax scrolling demo
 #
 function main {
-  $pf = create-playfield -x 0 -y 2 -width 80 -height 40 -bg "black"
+  $pf = create-playfield -x 0 -y 2 -width 78 -height 33 -bg "black" #78? multiple of 3
 
   $imga = create-image "'''","'''" -fg "blue" -bg "darkmagenta"
   $imgb = create-image "@@@","@@@" -fg "darkblue" -bg "darkmagenta"
-  $imgc = create-image "/=\","\=/" -fg "white" -bg "darkred"
 
-  $map = @{"A"=$imga; "B"=$imgb; "C"=$imgc}
+  $imgc = create-image "{|}","{:}" -fg "red" -bg "darkred"
+  $imgd = create-image "/=\","\=/" -fg "yellow" -bg "red"
+  $imge = create-image "\=/","/=\" -fg "yellow" -bg "red"
+  
+  $imgx = create-image "<=>"," W " -fg "magenta" -bg "black"
+  $imgy = create-image " | ","/%\" -fg "yellow" -bg "black"
 
-  $backlines  = "                                                                    ",
-                "BBA       BBA     BBBA               BBBBBBBA       BBA     BBBA    ",
-                "BBBBA   BBBBBA   BBBBBA             BBBBBBBBBBA   BBBBBA   BBBBBA   ",
+  $map = @{"A"=$imga; "B"=$imgb; "C"=$imgc; "D"=$imgd; "E"=$imge; "X"=$imgx; "Y"=$imgy}
+
+  $backlines  = "                   BA                                        BA     ",
+                "BBA        BA      BBA                BBBBBBA        BA      BBA    ",
+                "BBBBA    BBBBA   BBBBBA              BBBBBBBBBA    BBBBA   BBBBBA   ",
                 "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"
 
-  $frontlines = "     C         C       C                     C         C       C  ", 
-                "    CCC        C       C                    CCC        C       C  ",  
-                "     C        CC       CC                    C        CC       CC ",
-                "     C        CCC     CCCC                   C        CCC     CCCC"
+  $frontlines = "     C         D       E                     C         D       E  ", 
+                "    CCC       YC       C                    CCC       YC       C  ",  
+                "     C        CC    Y  CC     Y              C        CC    Y  CC ",
+                "     CCC  X   CCC   CCCCCC X CCC  Y          CCC  X   CCC   CCCCCC",
+                "CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC"
 
   $backtm = create-tilemap $backlines $map 3 2
   $fronttm = create-tilemap $frontlines $map 3 2
@@ -39,6 +46,17 @@ function main {
   #wrap points
   [int]$backwp = $backtm.tilewidth * $backtm.mapwidth-78
   [int]$frontwp = $backtm.tilewidth * $fronttm.mapwidth-78
+
+  # create sprites to complete the look
+  $thrustimg1 = create-image @(">") -fg "yellow" -bg "black"
+  $thrustimg2 = create-image @("=") -fg "red" -bg "black"
+  $rocketimg = create-image " o  ","D#>-" -fg "white" -bg "black"
+
+  $thrustsprite = create-sprite -images $thrustimg1,$thrustimg2 -x 4 -y 9
+  $rocketsprite = create-sprite -images @($rocketimg) -x 5 -y 8
+
+  $sprites = $thrustsprite,$rocketsprite
+
   # game loop
   [int]$fc = 0;
   while ($true) {
@@ -48,9 +66,10 @@ function main {
 
     clear-playfield $pf
     draw-tilemap $pf $backtm -offsetx $bx -offsety 0 -x 0 -y 25 -w 78 -h 12
-    draw-tilemap $pf $fronttm -offsetx $fx -offsety 0 -x 0 -y 25 -w 78 -h 12
+    draw-tilemap $pf $fronttm -offsetx $fx -offsety 0 -x 0 -y 24 -w 78 -h 15
+    draw-sprites $pf $sprites
     flush-playfield $pf
-    sleep -millis 2
+    sleep -millis 2 # wish this could be negative :-)
   }
 }
 
