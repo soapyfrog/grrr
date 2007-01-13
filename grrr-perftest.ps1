d4706f8aaec41c05a97d67ca2ec0e32a9123a564
adrian <adrian@91d854f5-e528-0410-816d-da5f51aa4fc4>
2007-01-13 12:01:58 +0000

    Adding a performance test script

---
+++ grrr-perftest.ps1
@@ -0,0 +1,108 @@
+#------------------------------------------------------------------------------
+# Copyright 2006 Adrian Milliner (ps1 at soapyfrog dot com)
+# http://ps1.soapyfrog.com
+#
+# This work is licenced under the Creative Commons 
+# Attribution-NonCommercial-ShareAlike 2.5 License. 
+# To view a copy of this licence, visit 
+# http://creativecommons.org/licenses/by-nc-sa/2.5/ 
+# or send a letter to 
+# Creative Commons, 559 Nathan Abbott Way, Stanford, California 94305, USA.
+#------------------------------------------------------------------------------
+
+# $Id$
+
+# performance test cases for grrr.ps1
+# run this as a script - do not 'source' it with '.'
+
+$global:___globcheck___=1
+$local:___globcheck___=2
+if ($global:___globcheck___ -eq 2) {throw "This should not be sourced in global scope"}
+
+$ErrorActionPreference="Stop" # endless errors annoy me
+
+# load modules
+. .\psunit.ps1
+. .\grrr.ps1
+
+$Kwidth = 80
+$Kheight = 50
+$Kiters = 150
+
+# init console
+init-console -w $Kwidth -h $Kheight 
+
+#----------------------------------------------------------------
+# test playfield alone
+function test-playfield-alone {
+  $pf = create-playfield -x 0 -y 24 -width 34 -height 20 -bg "darkblue" 
+  for ([int]$iter=$Kiters; $iter -gt 0; $iter--) {
+    clear-playfield $pf
+    flush-playfield $pf
+  }
+}
+
+
+#----------------------------------------------------------------
+# test drawing opaque images
+function test-draw-image-opaque {
+  $pf = create-playfield -x 0 -y 24 -width 34 -height 20 -bg "darkblue" 
+  $dragontxt = @"
+      .==.        .==.
+     //'^\\      //^'\\
+    //x^x^\(\__/)/^x^^\\
+   //^x^^x^/6xx6\x^^x^^\\
+  //^x^^x^x(x..x)x^x^^^x\\
+ //x^^x^/\//v""v\\/\^x^x^\\
+//x^^/\/  /x'~~'x\  \/\^x^\\
+\\^x/    /x,xxxx,x\    \^x//
+ \\/    (x(xxxxxx)x)    \//
+  ^      \x\.__./x/      ^
+         ((('  ')))
+"@
+  # replace the x with spaces, well dots
+  $dragontxt = $dragontxt.replace("x",[string][char]0x00b7)
+  # split into lines
+  $dragonlines = $dragontxt.replace("`r","W").replace("`n","").split("W")
+  $opaquedragon = create-image $dragonlines -fg "red" -bg "darkred"
+
+  for ([int]$iter=$Kiters; $iter -gt 0; $iter--) {
+    clear-playfield $pf
+    draw-image $pf $opaquedragon 1 6
+    flush-playfield $pf
+  }
+}
+#----------------------------------------------------------------
+# test drawing transparent images
+function test-draw-image-transparent {
+  $pf = create-playfield -x 0 -y 24 -width 34 -height 20 -bg "darkblue" 
+  $dragontxt = @"
+      .==.        .==.
+     //'^\\      //^'\\
+    //x^x^\(\__/)/^x^^\\
+   //^x^^x^/6xx6\x^^x^^\\
+  //^x^^x^x(x..x)x^x^^^x\\
+ //x^^x^/\//v""v\\/\^x^x^\\
+//x^^/\/  /x'~~'x\  \/\^x^\\
+\\^x/    /x,xxxx,x\    \^x//
+ \\/    (x(xxxxxx)x)    \//
+  ^      \x\.__./x/      ^
+         ((('  ')))
+"@
+  # replace the x with spaces, well dots
+  $dragontxt = $dragontxt.replace("x",[string][char]0x00b7)
+  # split into lines
+  $dragonlines = $dragontxt.replace("`r","W").replace("`n","").split("W")
+  $transparentdragon = create-image $dragonlines -fg "yellow" -bg "darkgreen" -transparent 32
+
+  for ([int]$iter=$Kiters; $iter -gt 0; $iter--) {
+    clear-playfield $pf
+    draw-image $pf $transparentdragon 1 6
+    flush-playfield $pf
+  }
+}
+
+
+#----------------------------------------------------------------
+# hand over to unit test framework
+run-tests
