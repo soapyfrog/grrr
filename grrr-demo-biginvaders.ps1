ebb7d8088cd1bc12891e26a535d5a6af0dd78fa1
adrian <adrian@91d854f5-e528-0410-816d-da5f51aa4fc4>
2007-01-18 16:08:55 +0000

    not happy with demo.. leaving it for now

--- grrr-demo-biginvaders.ps1
+++ grrr-demo-biginvaders.ps1
@@ -19,29 +19,35 @@ $global:___globcheck___=1
 $local:___globcheck___=2
 if ($global:___globcheck___ -eq 2) {throw "This should not be sourced in global scope"}
 
+$ErrorActionPreference="Stop"
 
 # load modules
 . .\grrr.ps1
+
+[int]$script:pfwidth = 256
+[int]$script:pfheight = 130
 
 init-console 256 130
 write-host "big space invaders demo - using original graphics!!"
 
 #------------------------------------------------------------------------------
 # Create the invader sprites
-# Returns a tuple of the direction object and the sprite array
+# Returns a tuple of the shared direction controller object and the 
+# sprite array
 #
 function create-invadersprites($images) {
   $sprites = @()
   # handlers for motion
-  $dir = @{current="R"; next="R"} # shared state for all sprites
+  $controller = @{current="R"; next="R"} # shared controller for all sprites
   $init = {
-    $args[0].dir = $dir
+    $args[0].controller = $controller
+    $args[0].tagged = $false # only the tagged invader moves
   }
   $move = { 
     $s=$args[0]
     $dx = 1
     $dy = 6
-    $d = $s.dir
+    $d = $s.controller
     switch ($d.current) {
       "R" {
         $s.x+=$dx
@@ -74,20 +80,49 @@ function create-invadersprites($images) {
     $xo -= 1
     $y += 13
   }
-  return $dir,$sprites
+  return $controller,$sprites
 }
 
-function main {
-  $images = get-images "grrr-demo-biginvaders-images.txt"
-  $dir,$sprites = create-invadersprites $images
 
-  # now create a field of aliens
+#------------------------------------------------------------------------------
+# create base ship sprite
+#
+function create-basesprite {
+  param($images)
+  $handlers = create-spritehandlers -didinit {
+    $s=$args[0]
+    $s.x = 30; $s.y=100
+    $s.dx = 0
+  } -willdraw {
+    $s=$args[0]
+    $s.x += $s.dx
+  }
+  $base = create-sprite -images $images.base -handlers $handlers
+  return $base
+}
+
+
+#------------------------------------------------------------------------------
+# demo starts here
+#
+function main {
+  # create a plafield to put it all on
   $pf = create-playfield -x 0 -y 2 -width 256 -height 128 -bg "black"
+
+  # load the alien images from the file
+  $images = get-images "grrr-demo-biginvaders-images.txt"
+  # create an alien hoard (well, a small gathering) 
+  $aliens_controller,$aliens = create-invadersprites $images
+  # create a base ship
+  $base = create-basesprite $images
+
 
   while ($true) {
     clear-playfield $pf
-    draw-sprites $pf $sprites
-    $dir.current = $dir.next
+    draw-sprites $pf $aliens
+    $aliens_controller.current = $aliens_controller.next
+    draw-sprite $pf $base
+    draw-string $pf "$duhidx  $duh   " 0 0
     flush-playfield $pf -sync 40
   }
 }
