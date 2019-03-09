595eccc5d6725317a58c1c74abfd53414289e0d0
Adrian Milliner <adrian@soapyfrog.com>
2019-03-09 10:56:49 +0000

    Fiddling with make installer

--- dist/makeinstaller.ps1
+++ dist/makeinstaller.ps1
@@ -4,15 +4,14 @@ $ErrorActionPreference="stop"
 
 
 
-cd 'C:\Documents and Settings\adrian\My documents\Projects\grrr-trunk\demos\biginvaders'
 
 $registered = Get-PSSnapin -registered | where {$_.name -ieq "soapyfrog.grrr"}
 if ($reregister -or !$registered) {
   if (!$registered) { write-host -f cyan "Not currently registered." }
   write-host -f cyan "Registering..." 
-  $iupath = (resolve-path $env:windir\Microsoft.NET\Framework\v2*\installutil.exe | sort path | select -last 1)
+  $iupath = (resolve-path $env:windir\Microsoft.NET\Framework\v4*\installutil.exe | sort path | select -last 1)
   if (!$iupath) { write-error "Can't find installutil.exe" }
-  & $iupath "C:\Documents and Settings\adrian\My Documents\Projects\grrr-trunk\grrr-snapin-solution\grrr-snapin\bin\Release\Soapyfrog.Grrr.dll" }
+  & $iupath "..\grrr-snapin-solution\grrr-snapin\bin\Release\Soapyfrog.Grrr.dll" }
 
 $added = Get-PSSnapin | where {$_.name -ieq "soapyfrog.grrr" }
 if (!$added) {
